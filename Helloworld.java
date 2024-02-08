7402621193e864ad0d3f03be6a776d9d6341247e
Frankie Fox Wagoosh <frank.fox-wagoosh@stu.ocsb.ca>
2024-02-08 15:31:31 +0000

    added Helloworld.java

---
+++ Helloworld.java
@@ -0,0 +1,15 @@
+/**
+* My program prints out hello world when ran
+* Frankie Fox 
+* 1.0
+*  Thursday, February, 8, 2024 
+*/
+// this prints out Hello world!
+// This also prints out Hi Tomi how are you doing?
+
+public class Helloworld {
+     public static void main(String[] args) {
+       System.out.println("Hello world!");
+       System.out.println("Hi Tomi how you doing?");
+  } 
+}
